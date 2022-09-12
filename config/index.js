f0ede9a6382ef463ff4b71c0dc85604fc7e7e7f9
LANKA166 <113319544+LANKA166@users.noreply.github.com>
2022-09-12 09:57:05 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -25,9 +25,9 @@ export const config = {
       // 想要发送的人的名字
       name: "包包",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "o7yyO540mcYKTBmYl4lhUR_9EQLY",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "yEWgsC2GxFGNtzaVb0DxuxfxWqNohWYWgbcB1nMPzsQ",
       // 所在省份
       province: "河南",
       // 所在城市
