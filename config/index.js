c99cabeb17972ef12f74692d2a27c4a65a7beb60
LANKA166 <113319544+LANKA166@users.noreply.github.com>
2022-09-12 09:19:50 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "爱包包的每一天",
+  APP_ID: "wx87a298eb6cf679f7",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "爱包包的每一天",
+  APP_SECRET: "137bf7afe11a57c43ac07f8366ce0ae0",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
